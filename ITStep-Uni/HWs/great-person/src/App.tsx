647c18bea025b88448e473bfa41d28c8ca5117d9
DeNt <mrfraos@gmail.com>
2024-06-03 08:40:05 +0300

    HW's

---
+++ ITStep-Uni/HWs/great-person/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Info from './components/Info/Info'
+
+export default function App() {
+  return (
+    <div className = "md:container flex h-96">
+      <Info></Info>
+    </div>
+  );
+}
---
+++ ITStep-Uni/HWs/great-person/src/components/Info/Info.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Info () {
+
+    return (
+
+        <div className = "flex-col w-24 h-screen justify-center items-center bg-slate-800">
+            <p>Hello</p>
+        </div>
+    );
+}
